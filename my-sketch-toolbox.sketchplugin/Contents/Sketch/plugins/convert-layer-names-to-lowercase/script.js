35ea709567b04dfa796baeba699ec1c9ad2db8af
Peter Berrecloth <peter.berrecloth@kuluvalley.com>
2016-09-19 18:40:09 +0100

    Add ConvertLayerNamesToLowercase

---
+++ my-sketch-toolbox.sketchplugin/Contents/Sketch/plugins/convert-layer-names-to-lowercase/script.js
@@ -0,0 +1,15 @@
+var convertLayerNamestoLowercase = function(context) {
+
+    var selection = context.selection
+
+    for (var i=0; i<selection.count(); i++) {
+
+        var layer = selection[i]
+
+        // convert to lowercase
+        
+        }
+    }
+
+    context.document.reloadInspector()
+};
